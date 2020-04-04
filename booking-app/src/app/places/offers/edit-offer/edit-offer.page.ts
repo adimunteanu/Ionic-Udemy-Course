81fa564a66a4a83ad0ba887e16bd6c14bbae0137
Adrian Munteanu <adimunteanu13@gmail.com>
2020-04-04 11:12:39 +0200

    feat: added edit offer form

--- booking-app/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ booking-app/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -3,6 +3,7 @@ import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -11,6 +12,7 @@ import { NavController } from '@ionic/angular';
 })
 export class EditOfferPage implements OnInit {
   loadedPlace: Place;
+  form: FormGroup;
 
   constructor(
     private placesService: PlacesService,
@@ -25,7 +27,23 @@ export class EditOfferPage implements OnInit {
         return;
       }
       this.loadedPlace = this.placesService.getPlace(paramMap.get('placeId'));
+      this.form = new FormGroup({
+        title: new FormControl(this.loadedPlace.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.loadedPlace.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
     });
   }
 
+  onUpdateOffer() {
+    if(!this.form.valid){
+      return;
+    }
+    console.log(this.form);
+  }
 }
