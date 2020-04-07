6fa3ad218f74e12d139ec8727a66f3f1256f913a
Adrian Munteanu <adimunteanu13@gmail.com>
2020-04-07 16:44:22 +0200

    feat: added http get request functionality

--- booking-app/src/app/places/places.service.ts
+++ booking-app/src/app/places/places.service.ts
@@ -6,42 +6,54 @@ import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { isBuffer } from 'util';
 import { HttpClient } from '@angular/common/http';
 
+
+// new Place(
+//   'p1',
+//   'Manhattan Mansion',
+//   'In the heart of New York City.',
+//   'https://static.mansionglobal.com/production/media/article-images/8b6775fa0db27ccc4a5aa3fe8d13f8cc/large_053.jpg',
+//   149.99,
+//   new Date('2019-01-01'),
+//   new Date('2019-12-31'),
+//   'abc'
+// ),
+// new Place(
+//   'p2',
+//   'L\'Amour Toujours',
+//   'Romantic place in Paris.',
+//   'https://i.pinimg.com/originals/a9/45/72/a945725c0dc12770bb831e8d9fbc2fe6.png',
+//   189.99,
+//   new Date('2019-01-01'),
+//   new Date('2019-12-31'),
+//   'abc'
+// ),
+// new Place(
+//   'p3',
+//   'The Foggy Palace',
+//   'Not your average city trip!',
+//   'https://data.whicdn.com/images/292751032/original.jpg',
+//   99.99,
+//   new Date('2019-01-01'),
+//   new Date('2019-12-31'),
+//   'xyz'
+// )
+
+
+interface PlaceData {
+  availableFrom: string;
+  availableTo: string;
+  description: string;
+  imageUrl: string;
+  price: 1212;
+  title: string;
+  userId: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York City.',
-      'https://static.mansionglobal.com/production/media/article-images/8b6775fa0db27ccc4a5aa3fe8d13f8cc/large_053.jpg',
-      149.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p2',
-      'L\'Amour Toujours',
-      'Romantic place in Paris.',
-      'https://i.pinimg.com/originals/a9/45/72/a945725c0dc12770bb831e8d9fbc2fe6.png',
-      189.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p3',
-      'The Foggy Palace',
-      'Not your average city trip!',
-      'https://data.whicdn.com/images/292751032/original.jpg',
-      99.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'xyz'
-    )
-  ]);
+  private _places = new BehaviorSubject<Place[]>([]);
 
   get places() {
     return this._places.asObservable();
@@ -51,6 +63,34 @@ export class PlacesService {
     private authService: AuthService,
     private http: HttpClient
   ) { }
+
+  fetchPlaces() {
+    return this.http
+      .get<{ [key: string]: PlaceData }>('https://ionic-angular-booking-ap-5f860.firebaseio.com/offered-places.json')
+      .pipe(
+        map(resData => {
+          const places = [];
+          for (const key in resData) {
+            if (resData.hasOwnProperty(key)) {
+              places.push(new Place(
+                key,
+                resData[key].title,
+                resData[key].description,
+                resData[key].imageUrl,
+                resData[key].price,
+                new Date(resData[key].availableFrom),
+                new Date(resData[key].availableTo),
+                resData[key].userId
+              ));
+            }
+          }
+          return places;
+        }),
+        tap(places => {
+          this._places.next(places);
+        })
+      );
+  }
 
   getPlace(id: string) {
     return this.places.pipe(
