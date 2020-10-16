92fbb5290eff9ce6292378fc119c744fb6374036
Adrian Munteanu <adimunteanu13@gmail.com>
2020-10-16 10:12:37 +0200

    feat: add user model and behaviour subject

--- booking-app/src/app/auth/auth.service.ts
+++ booking-app/src/app/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -16,15 +19,30 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
-  private _userId = null;
+  private _user = new BehaviorSubject<User>(null);
 
   get userIsAuthenticated() {
-    return this._userIsAuthenticated;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return !!user.token;
+        } else {
+          return false;
+        }
+      })
+    );
   }
 
   get userId() {
-    return this._userId;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return user.id;
+        } else {
+          return null;
+        }
+      })
+    );
   }
 
   constructor(private http: HttpClient) { }
@@ -44,6 +62,6 @@ export class AuthService {
   }
 
   logout() {
-    this._userIsAuthenticated = false;
+    this._user.next(null);
   }
 }
